0a00cceaa147332c550f1c5f4b30fcd1c308d90b
linshukai <lsk@gzsunrun.cn>
2023-01-04 10:41:58 +0800

    feat: #1 新增并发的目录遍历示例

---
+++ ch8/du2/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var verbose = flag.Bool("v", false, "show verbose progress messages")
+
+func walkDir(dir string, fileSize chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			walkDir(subdir, fileSize)
+		} else {
+			fileSize <- entry.Size()
+		}
+	}
+}
+
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
+		return nil
+	}
+	return entries
+}
+
+func main() {
+	flag.Parse()
+
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	fileSize := make(chan int64)
+	go func() {
+		for _, root := range roots {
+			walkDir(root, fileSize)
+		}
+		close(fileSize)
+	}()
+
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
+	}
+
+	var nfiles, nbytes int64
+
+loop:
+	for {
+		select {
+		case size, ok := <-fileSize:
+			if !ok {
+				break loop
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	printDiskUsage(nfiles, nbytes)
+}
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("%d files %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
