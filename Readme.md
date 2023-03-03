0c4ac87f192fb7aa8ff5216c3dbc88aeb1615041
linshukai <lsk@gzsunrun.cn>
2023-03-03 11:07:11 +0800

    feat: #4 Go OrderMap使用

--- Readme.md
+++ Readme.md
@@ -1,7 +1,31 @@
-## 目录
+# 项目简介
 
-### Go语言圣经示例
+本项目是一个关于 Go 语言圣经、Gin 框架以及 Go 日常使用模块的示例代码库。
 
-### Gin框架使用示例
+代码示例涉及了 Go 语言基础、并发编程、网络编程、数据库操作、测试等多个方面。
 
-### Sqlx数据库操作示例
+本项目旨在帮助 Go 语言初学者快速上手并深入了解 Go 语言的使用。
+
+
+## 目录结构
+
+```cpp
+.
+|-- AssertionDemo
+|-- Demo
+|-- GinDemo
+|-- Readme.md
+|-- SqlxDemo
+|-- ch5
+|-- ch6
+|-- ch7
+|-- ch8
+|-- conf.json
+|-- go.mod
+|-- go.sum
+|-- main
+|-- main.exe
+|-- public
+`-- templates
+```
+
--- main/main.go
+++ main/main.go
@@ -1,42 +1,7 @@
 package main
 
-import "fmt"
-
-func testHello1(name string) error {
-
-	if len(name) > 10 {
-		return fmt.Errorf("输入的名字长度超过10")
-	}
-	return nil
-}
-
-func testHello2(age int) error {
-
-	if age > 200 {
-		return fmt.Errorf("正常人年龄不能超过200岁")
-	}
-	return nil
-}
-
-func testInput(name string) (text string, err error) {
-	if len(name) > 10 {
-		err = fmt.Errorf("输入的名字长度超过10")
-	}
-
-	text = "Hello, My name is " + name
-	return
-}
+import "sunrun/public"
 
 func main() {
-	err := testHello1("zhangsansdjfiuosjodifjaoisd")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if err = testHello2(300); err != nil {
-		fmt.Println(err)
-	}
-
-	text, err := testInput("Tom")
-	fmt.Println(text)
+	public.GenOrderMap()
 }
