31e6a0682045d7a698de78b5fa19853d2b04f36c
linshukai <lsk@gzsunrun.cn>
2023-03-17 14:23:37 +0800

    feat: #4 Go reflect的使用

--- go.mod
+++ go.mod
@@ -25,6 +25,9 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
--- main/main.go
+++ main/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "sunrun/public"
+import (
+	"fmt"
+)
+
+type info interface {
+	SayName() string
+	SayAge() int
+}
+
+type handle interface {
+	HandleInfo()
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p *Person) SayName() string {
+	return p.Name
+}
+
+func (p *Person) SayAge() int {
+	return p.Age
+}
+
+func (p *Person) SetName(n string) {
+	p.Name = n
+}
+
+func (p *Person) HandleInfo() {
+	fmt.Println("My Name is ", p.Name, "My Age is  ", p.Age)
+}
+
+func Hello() *Person {
+	fmt.Println("Hello,world")
+	return &Person{"zhangsna", 22}
+}
 
 func main() {
-	public.GenOrderMap()
+	p2 := &Person{}
+	p2.SetName("lisi")
+	fmt.Println(p2.SayName())
+
+	a := map[string]func() interface{}{
+		"zhangsan": func() interface{} { return Hello() },
+	}
+
+	fmt.Println(a)
+	fmt.Println(a["zhangsan"]())
+
 }
