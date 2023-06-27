5344833098caec8e6fcddd6cae44ab9110819217
linshukai <lsk@gzsunrun.cn>
2023-06-28 00:41:40 +0800

    feat: #4 更新FTP Demo

--- FtpDemo/ftpDemo.go
+++ FtpDemo/ftpDemo.go
@@ -2,37 +2,59 @@ package main
 
 import (
 	"fmt"
+	"github.com/jinzhu/configor"
 	"github.com/jlaffaye/ftp"
 	"log"
+	"os"
 )
 
-func main() {
-	ftpConn, err := ftp.Dial("126.0.0.1:21")
+type FtpConfig struct {
+	Ftp FtpConn `json:"ftp"`
+}
 
-	if err != nil {
-		log.Fatal(err.Error())
+type FtpConn struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+}
+
+func initFtpConn() (err error, ftpConn *ftp.ServerConn) {
+
+	confPath := "conf.json"
+
+	if _, err = os.Stat(confPath); err != nil {
+		return
 	}
 
-	err = ftpConn.Login("ftp", "123456")
+	var Config FtpConfig
+	if err = configor.Load(&Config, confPath); err != nil {
+		return
+	}
+
+	ftpConfig := Config.Ftp
+	ftpConn, err = ftp.Dial(fmt.Sprintf("%s:%d", ftpConfig.Host, ftpConfig.Port))
+	if err != nil {
+		return
+	}
+
+	err = ftpConn.Login(ftpConfig.Username, ftpConfig.Password)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func main() {
+	err, ftpConn := initFtpConn()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer func() {
-		fmt.Println("logout.....")
+	uploadFtpPath := "/home/ftpuser"
 
-		errFtp := ftpConn.Logout()
-		if errFtp != nil {
-			log.Fatal(errFtp.Error())
-		}
-		fmt.Println("Quit......")
-		errFtp = ftpConn.Quit()
-		if errFtp != nil {
-			log.Fatal(errFtp.Error())
-		}
-	}()
-
-	err = ftpConn.ChangeDir("ftp")
+	err = ftpConn.ChangeDir(uploadFtpPath)
 	if err != nil {
 		fmt.Println("mkdir .....")
 		err = ftpConn.MakeDir("ftp")
@@ -40,24 +62,44 @@ func main() {
 			log.Println(err.Error())
 		}
 		fmt.Println("changedir....")
-		err = ftpConn.ChangeDir("F://ftp")
+		err = ftpConn.ChangeDir("/home/ftpuser")
 		if err != nil {
 			log.Println(err.Error())
 		}
 	}
 
+	// 切换目录
 	fmt.Println("list.....")
 	err = ftpConn.ChangeDirToParent()
 	if err != nil {
 		log.Println(err)
 	}
 
-	files, err := ftpConn.List(".")
+	// 查看当前目录文件
+	//files, err := ftpConn.List(".")
+	//if err != nil {
+	//	log.Println(err.Error())
+	//}
+	//for _, file := range files {
+	//	fmt.Println(file.Name)
+	//}
+
+	fmt.Println("Upload")
+	// 上传文件
+	file, err := os.Open("conf.json")
 	if err != nil {
-		log.Println(err.Error())
-	}
-	for _, file := range files {
-		fmt.Println(file.Name)
+		log.Fatal(err)
 	}
 
+	defer file.Close()
+
+	err = ftpConn.Stor(fmt.Sprintf("%s//conf.json", uploadFtpPath), file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ftpConn.Quit()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
