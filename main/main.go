791ffd14744b0b5b0f9cd8e2af4a2e7530461800
root <root@LAPTOP-834NAHNP.localdomain>
2024-02-05 17:36:50 +0800

    docs: update go manual documents.

--- main/main.go
+++ main/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"sunrun/golib/httpdemo"
+	"fmt"
 )
 
+type linshukaiFloat float64
+
+func getError() (string, error) {
+	return "hello,world", fmt.Errorf("test error")
+}
+
+func getTmp() error {
+	return fmt.Errorf("tmp error")
+}
+
+func get() (err error) {
+	var err2 error
+	fmt.Printf("%p", &err2)
+	s, err2 := getError()
+	if err2 != nil {
+		fmt.Println(s)
+		fmt.Printf("%p", &err2)
+		fmt.Println(err2.Error())
+		return err2
+	}
+	return
+}
+
 func main() {
-	httpdemo.HandleHttpRequest()
+	// httpdemo.HandleHttpRequest()
+	a := linshukaiFloat(9.888)
+	fmt.Println(a)
+	b := float64(999.9)
+
+	c := linshukaiFloat(b)
+	fmt.Println(c)
 }
