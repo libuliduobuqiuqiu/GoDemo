370698f77f0cd92099a77146934627f5af208593
libuliduobuqiuqiu <mbb929833@gmail.com>
2024-02-21 17:39:37 +0800

    feat: add docs about go interface.

--- main/main.go
+++ main/main.go
@@ -2,17 +2,52 @@ package main
 
 import "fmt"
 
+type PointerInterface interface {
+	AddX()
+	PrintX()
+}
+
+type SubInterface interface {
+	SubX()
+}
+
 type Pointer struct {
 	X int
+	Y int
 }
 
 func (p *Pointer) AddX() {
 	p.X += 1
+}
+
+func (p *Pointer) SubX() {
+	p.X -= 1
+}
+
+func (p *Pointer) PrintX() {
 	fmt.Println(p.X)
 }
 
+func UsePointer(p PointerInterface) {
+	if tmp, ok := p.(*Pointer); ok {
+		tmp.Y += 1
+	}
+
+	if tmp, ok := p.(SubInterface); ok {
+		tmp.SubX()
+	}
+
+	switch p.(type) {
+	case Pointer:
+		p.PrintX()
+	case SubInterface:
+		p.SubX()
+	}
+}
+
 func main() {
-	p := Pointer{X: 2}
-	p.AddX()
+	p := &Pointer{X: 2}
+	UsePointer(p)
+	fmt.Println(p.Y)
 	fmt.Println(p.X)
 }
