7667469089be7da953cab4ff6ef1ac381f7e5f86
libuliduobuqiuqiu <mbb929833@gmail.com>
2024-06-13 17:43:08 +0800

    update: update reflect docs.

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
-	"godemo/internal/godemo"
-	"godemo/internal/gomanual/genericsdemo"
+	"reflect"
 )
 
-func main() {
-	type a int8
-	genericsdemo.PrintMan()
+func handleUser(u interface{}, newName string) {
+	tmp := reflect.ValueOf(u)
+	if tmp.Kind() == reflect.Ptr {
+		var newUser interface{}
+		user := tmp.Elem()
+		name := user.FieldByName("Name")
+		if name.IsValid() && name.CanSet() {
+			newUser = user.Interface()
+			name.SetString(newName)
+		}
+		fmt.Println(newUser)
+	}
+}
 
-	err := godemo.HandleError()
-	fmt.Println(err)
+func main() {
+	n := struct {
+		Name string `json:"name"`
+	}{Name: "linshukai"}
+	handleUser(&n, "new_linshukai")
+	fmt.Println(n.Name)
 }
