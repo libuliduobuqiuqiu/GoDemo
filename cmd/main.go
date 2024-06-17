7e0f222f30e537fd2973253517627ebd50dff992
libuliduobuqiuqiu <mbb929833@gmail.com>
2024-06-17 14:09:06 +0800

    feat: update context demo.

--- cmd/main.go
+++ cmd/main.go
@@ -2,27 +2,8 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
-func handleUser(u interface{}, newName string) {
-	tmp := reflect.ValueOf(u)
-	if tmp.Kind() == reflect.Ptr {
-		var newUser interface{}
-		user := tmp.Elem()
-		name := user.FieldByName("Name")
-		if name.IsValid() && name.CanSet() {
-			newUser = user.Interface()
-			name.SetString(newName)
-		}
-		fmt.Println(newUser)
-	}
-}
-
 func main() {
-	n := struct {
-		Name string `json:"name"`
-	}{Name: "linshukai"}
-	handleUser(&n, "new_linshukai")
-	fmt.Println(n.Name)
+	fmt.Println("hello, welcom to godemo project.")
 }
---
+++ internal/goconcurrency/gocontext/withvalue_demo.go
@@ -0,0 +1,40 @@
+package gocontext
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+func handleZhangsan(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	ctx = context.WithValue(ctx, "name1", "zhangsan")
+	fmt.Println("name1: ", ctx.Value("name1"))
+}
+
+func handleLining(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	ctx = context.WithValue(ctx, "name1", "linsss")
+	fmt.Println("name2: ", ctx.Value("name1"))
+}
+
+// 测试context的是否线程安全
+// conetxt线程安全，因为每个使用withvalue的相当于套了一层Context
+// 获取值的时候递归沿着context寻找value
+func HandlerUserValue() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "name", "linshukai")
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		handleZhangsan(ctx, wg)
+	}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		handleLining(ctx, wg)
+	}
+
+	wg.Wait()
+}
