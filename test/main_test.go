a9e63a39d2b91f8af2a143996d802c6d2fa7c660
libuliduobuqiuqiu <mbb929833@gmail.com>
2024-07-18 17:45:02 +0800

    update: update map docs.

--- test/main_test.go
+++ test/main_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"fmt"
 	"testing"
+	"unsafe"
 )
 
 type MainPerson struct {
@@ -45,4 +46,46 @@ func TestReturnStruct(t *testing.T) {
 	data := ReturnStruct()
 	fmt.Printf("%p\n", &data)
 	return
+}
+
+func TestSliceAppend(t *testing.T) {
+	var b []int = nil
+	fmt.Printf("%p\n", b)
+	fmt.Println(len(b), cap(b))
+
+	b = append(b, 100)
+	b = append(b, 200, 200, 200, 200, 200, 200)
+	fmt.Printf("%p\n", b)
+	fmt.Println(len(b), cap(b))
+
+	s := make([]int, 2, 2)
+	s[0] = 1
+	s[1] = 2
+
+	// 打印初始切片的地址和底层数组的地址
+	fmt.Printf("Initial slice address: %p\n", s)
+	fmt.Printf("Initial array address: %p\n", unsafe.Pointer(&s[0]))
+
+	// 添加元素，触发扩容
+	s = append(s, 3)
+
+	// 打印扩容后的切片地址和底层数组的地址
+	fmt.Printf("New slice address: %p\n", s)
+	fmt.Printf("New array address: %p\n", unsafe.Pointer(&s[0]))
+}
+
+func ChangeSlice(a []int) []int {
+	a[1] = 99999
+	a = append(a, 100000)
+	fmt.Println(len(a), cap(a))
+	return a
+}
+
+func TestSliceSend(t *testing.T) {
+
+	a := []int{10, 10}
+	a = append(a, 10)
+	b := ChangeSlice(a)
+	fmt.Println(a, b)
+	fmt.Println(len(a), cap(a))
 }
